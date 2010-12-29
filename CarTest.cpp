fe1013bdc3bb87ac71653a112bd1e34b1d282d78
Qun Fa <testforqunfa@gmail.com>
2010-12-29 00:08:23 -0500

    add test for drive

--- CarTest.cpp
+++ CarTest.cpp
@@ -6,3 +6,12 @@ TEST(CarTest, DefaultConstructor) {
   EXPECT_EQ(0, car.getMiles());
   EXPECT_EQ("Porsche 911 Carrera", car.getName());
 }
+
+TEST(CarTest, DriveTest) {
+  Car car("Porsche 911 Carrera");
+  EXPECT_EQ(0, car.getMiles());
+  car.drive(10);
+  EXPECT_EQ(10, car.getMiles());
+  car.drive(20);
+  EXPECT_EQ(30, car.getMiles());
+}
